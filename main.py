dc6aa9eb91ef4eb76f14ef5729419a30ddfb4e1a
eric-sims <e.sims@icloud.com>
2021-06-30 20:52:47 -0600

    Options work for the program. The class can store the reports for each day.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 #/usr/bin/python3
 
+from enum import Enum
 import sys
 import argparse
 import datetime
@@ -9,22 +10,19 @@ import xml.sax
 class StatusReport(xml.sax.ContentHandler):
     def __init__(self) -> None:
         self.monday_date : datetime
-        self.messages = {
-            'Monday'    : "",
-            'Tuesday'   : "",
-            'Wednesday' : "",
-            'Thursday'  : "",
-            'Friday'    : "",
-        }
+        self.messages = [ "", "", "", "", "", "", ""]
         self.need_help = ""
         
+    def hasMessage(self, day) -> bool:
+        return len(self.messages[day]) > 0
+
     def setDate(self) -> None:
         self.monday_date = datetime.datetime.now()
 
     def recordMessage(self, day, message):
+        assert(day < 8)
         self.messages[day] = message
 
-    
 
 arg_parser = argparse.ArgumentParser(description="Filling out the Weekly Status Report")
 arg_parser.add_argument("-m", "--message", help="Reflect on the day", action="store_true")
@@ -36,6 +34,16 @@ group.add_argument("-q", "--quiet", action="store_true")
 
 args = arg_parser.parse_args()
 
+def prompt():
+    print( """************** Launchie Report Filler ****************
+    w - Reflections for today's work
+    p - Reflections for another weekday
+    f - Finish the document
+    e - Email report to supervisor
+    h - help
+    q - quit
+******************************************************""")
+
 
 if __name__ == "__main__":
     #import from [xml/JSON] file
@@ -46,13 +54,46 @@ if __name__ == "__main__":
     # Add report of previous date
     # Add comments of Planned accomplishments for next week
     # Add comments of Requested Assistance
-    
+
+    # Create Status Report object
+    report = StatusReport()
     currentDate = datetime.datetime.now()
+    weekday = currentDate.today().weekday()
+    cmd : str
 
-    if args.message:
-        if args.verbose:
-            print("message")
-            print(currentDate.strftime("%x"))
+    prompt()
+    while (1):
+        cmd = input(">>> ")
+        if cmd == 'q': break
+        elif cmd == 'w':
+            # print ("response for today")
+            if report.hasMessage(weekday): 
+                print("Already filled!")
+            else: 
+                reflection = input("enter reflection: ")
+                report.recordMessage(weekday, reflection)
+            print(report.messages)
 
-    elif args.email:
-        if args.verbose: print("email")
+        elif cmd == 'p':
+            # print("response for previous days")
+            custom_day = int(input("0 - Monday\n1 - Tuesday\n2 - Wednesday\n3 - Thursday\n4 - Friday\n5 - Saturday\n6 - Sunday\nday: "))
+            if report.hasMessage(custom_day): 
+                print("Already filled!")
+            else: 
+                reflection = input("enter reflection: ")
+                report.recordMessage(custom_day, reflection)
+        elif cmd == 'e':
+            print("emailing supervisor")
+        elif cmd == 'f':
+            print("finishing documentation")
+        elif cmd == 'h':
+            prompt()
+        
+
+    # if args.message:
+    #     if args.verbose:
+    #         print("message")
+    #         print(currentDate.strftime("%x"))
+
+    # elif args.email:
+    #     if args.verbose: print("email")
